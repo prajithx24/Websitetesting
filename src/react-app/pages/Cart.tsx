7c6e7eec04c2eb897fe38a0f429cc0f3ee41bfd6
Prajith P <prajith@gmail.com>
2025-08-21 00:40:57 +0530

    Fix: Revert Cart.tsx import to react-router to fix build error

--- src/react-app/pages/Cart.tsx
+++ src/react-app/pages/Cart.tsx
@@ -1,5 +1,5 @@
 import { Minus, Plus, Trash2, ShoppingBag, ArrowLeft } from 'lucide-react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import { useCart } from '../contexts/CartContext';
 import WhatsAppButton from '../components/WhatsAppButton';
 
