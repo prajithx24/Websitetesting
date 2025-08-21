0b910bd5c9cb2f3d5622f426b1df107f2b28756b
Dazzal David <91616790+dazzal-david@users.noreply.github.com>
2025-08-21 20:53:35 +0530

    Add files via upload

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "@/react-app/components/Navbar";
 import Footer from "@/react-app/components/Footer";
 import ScrollToTop from "@/react-app/components/ScrollToTop";
